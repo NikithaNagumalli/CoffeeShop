81480f0c2a2414ac2c87ec2fe07627dcde3066f5
NikithaNagumalli <nikithanagumalli@yahoo.com>
2026-03-01 08:40:31 -0600

    CancellationToken

--- CoffeeShop/Program.cs
+++ CoffeeShop/Program.cs
@@ -3,64 +3,40 @@ await coffeeShop.Customer();
 
 public class CoffeeShop
 {
-    // brew espresso - 2 seconds
-    // toast panini - 4 seconds
-
-    // public string BrewEspresso()
-    // {
-    //     Console.WriteLine("Brewing Espresso");
-    //     Thread.Sleep(2000);
-    //     Console.WriteLine("Brewed Espresso");
-    //     return "Made Espresso";
-    // }
-    //
-    // public string ToastPanini()
-    // {
-    //     Console.WriteLine("Toasting Panini");
-    //     Thread.Sleep(4000);
-    //     Console.WriteLine("Toasted Panini");
-    //     return "Made Panini";
-    // }
-    //
-    // public void Customer()
-    // {
-    //     Task task1 = new Task(() => { BrewEspresso(); });
-    //     Task task2 = new Task(() => { ToastPanini(); });
-    //     task1.Start();
-    //     task2.Start();
-    //     
-    //     Console.WriteLine("doing other stuff");
-    //     
-    //     Task.WaitAll(task1, task2);
-    //     return;
-    // }
-    
-    
-    public async Task<string> BrewEspresso()
+    public async Task<string> BrewEspresso(CancellationToken token)
     {
         Console.WriteLine("Brewing Espresso");
-        await Task.Delay(2000);
+        await Task.Delay(2000, token);
         Console.WriteLine("Brewed Espresso");
         return "Made Espresso";
     }
     
-    public async Task<string> ToastPanini()
+    public async Task<string> ToastPanini(CancellationToken token)
     {
         Console.WriteLine("Toasting Panini");
-        await Task.Delay(4000);
+        await Task.Delay(4000, token);
         Console.WriteLine("Toasted Panini");
         return "Made Panini";
     }
     
     public async Task Customer()
     {
-        Task<string> task1 = BrewEspresso();
-        Task<string> task2 = ToastPanini();
+        CancellationTokenSource cts = new CancellationTokenSource(3000);
+        try
+        {
+            Task<string> task1 = BrewEspresso(cts.Token);
+            Task<string> task2 = ToastPanini(cts.Token);
+
+            Console.WriteLine("doing other stuff");
+
+            string response1 = await task1;
+            string response2 = await task2;
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Operation cancelled");
+        }
         
-        Console.WriteLine("doing other stuff");
-        
-        string response1 = await task1;
-        string response2 = await task2;
         
         return;
     }
--- CoffeeShop/obj/Debug/net10.0/CoffeeShop.AssemblyInfo.cs
+++ CoffeeShop/obj/Debug/net10.0/CoffeeShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1df5a91a2738ba5596c49c668812d85589fb37")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
